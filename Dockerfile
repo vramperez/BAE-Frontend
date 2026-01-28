fd4e5b2d8cb18531ac40a48e46b782c32eaba5fc
Víctor Rampérez Martín <vramperez@MacBook-Air-de-Victor.local>
2026-01-28 15:36:11 +0100

    improve o-cei theme

---
+++ Dockerfile
@@ -0,0 +1,3 @@
+FROM vramperezseamware/biz-ecosystem-logic-proxy:10.7.0-ocei-theme
+
+COPY ./dist/bae-frontend /opt/business-ecosystem-logic-proxy/portal/bae-frontend
--- cypress/support/constants.ts
+++ cypress/support/constants.ts
@@ -19,7 +19,7 @@ export const init_config = {
     'matomoUrl': '',
     'searchEnabled': false,
     'domeAbout': 'https://dome-marketplace.eu/about/',
-    'domeRegister': 'https://dome-marketplace.github.io/onboarding/',
+    'domeRegister': '/',
     'domePublish': 'https://knowledgebase.dome-marketplace.org/shelves/company-onboarding-process',
     'purchaseEnabled': false,
     'defaultId': 'urn:ngsi-ld:catalog:32828e1d-4652-4f4c-b13e-327450ce83c6',
